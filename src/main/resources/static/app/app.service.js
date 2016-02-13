d993a821775fbce7cad62a846b7f3e608c1a2176
Raphael Nascimento <raphaelln@gmail.com>
2016-02-12 22:28:00 -0200

    services

--- src/main/resources/static/app/app.service.js
+++ src/main/resources/static/app/app.service.js
@@ -1,31 +1,31 @@
 'use strict';
 
 acmeApp.factory('Country',function($resource){
-    return $resource('http://localhost:8080/api/country/:id',{id:'@id'},{
+    return $resource('api/country/:id',{id:'@id'},{
         update: {
             method: 'PUT'
         }
     });
 }).factory('City',function($resource){
-    return $resource('http://localhost:8080/api/city/:id',{id:'@id'},{
+    return $resource('api/city/:id',{id:'@id'},{
         update: {
             method: 'PUT'
         },
         loadByState: {
-        	method: 'GET', url: 'http://localhost:8080/api/city/state/:stateId', isArray: true
+        	method: 'GET', url: 'api/city/state/:stateId', isArray: true
         }
     });
 }).factory('State',function($resource){
-    return $resource('http://localhost:8080/api/state/:id',{id:'@id'},{
+    return $resource('api/state/:id',{id:'@id'},{
         update: {
             method: 'PUT'
         },
         loadByCountry: {
-        	method: 'GET', url: 'http://localhost:8080/api/state/country/:countryId', isArray: true
+        	method: 'GET', url: 'api/state/country/:countryId', isArray: true
         }
     });
 }).factory('Company',function($resource){
-	return $resource('http://localhost:8080/api/company/:id',{id:'@id'},{
+	return $resource('api/company/:id',{id:'@id'},{
 		update: {
 			method: 'PUT'
 		}
